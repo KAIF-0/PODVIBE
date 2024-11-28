20c75f85eef4b18eb03bee1d6515a45f93afcd61
KAIF 0 <kaif8700979251@gmail.com>
2024-11-28 20:57:14 +0530

    toast message edited

--- src/app/api/(youtube)/(stream)/live-streams/route.js
+++ src/app/api/(youtube)/(stream)/live-streams/route.js
@@ -11,7 +11,7 @@ export async function GET(request) {
 
   if (!access_token) {
     return NextResponse.json(
-      { error: "Token is missing" },
+      { error: "User is not authenticated!" },
       {
         status: 401,
       }
